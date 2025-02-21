1c8e8d17f346f3cff999c66917e2f0e760c809c4
matcro02 <matthew.crouch@arm.com>
2025-02-21 10:26:51 +0000

    DNN: Building more structure for FineTuning pipeline
    The aim is to build a flexible class that enables fine tuning
    workloads to be deployed and run for a variety of tasks and
    workloads

--- src/fine_tuning_pipeline.py
+++ src/fine_tuning_pipeline.py
@@ -3,26 +3,78 @@
 from enum import Enum
 
 from datasets import DatasetDict
+from pydantic import BaseModel
 from transformers import (
     AutoModelForCausalLM,
+    AutoModelForSeq2SeqLM,
     AutoModelForSequenceClassification,
     AutoTokenizer,
 )
 
 
-class ModelType(Enum):
+class TaskType(Enum):
     """Enum class for model types."""
 
-    sequence_classification = AutoModelForSequenceClassification
-    text_generation = AutoModelForCausalLM
+    SEQUENCE_CLASSIFICATION = "sequence_classification"
+    TEXT_GENERATION = "text_generation"
+    TEXT_SUMMARISATION = "summarisation"
+
+
+class FineTuningConfig(BaseModel):
+    """Dataclass for fine-tuning configuration."""
+
+    text_column: str
+    target_column: str
+    lora: bool = False
+    quantisation: bool = False
 
 
 class FineTunerPipeline:
-    """Fine-tuning pipeline for transformers models."""
+    """Fine-tuning pipeline for transformers models.
 
-    def __init__(self, model_name: str, dataset: DatasetDict, mode: ModelType):
+    This pipeline supports and simplifies fine tuning transformer models
+    for specific tasks. The pipeline supports sequence classification,
+    text generation, and summarisation tasks.
+    """
+
+    def __init__(
+        self,
+        model_name: str,
+        dataset: DatasetDict,
+        mode: TaskType,
+        fine_tuning_config: dict,
+    ):
         """Initialize the fine-tuning pipeline."""
         self.model_name = model_name
         self.dataset = dataset
+        transformer_model = self._mode_options.get(mode)
+
+        self.fine_tuning_config = FineTuningConfig(**fine_tuning_config)
+
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        self.model = mode.from_pretrained(self.model_name)
+
+        # This from_pretrained needs to be able to support arbitary kwargs
+        self.model = transformer_model.from_pretrained(self.model_name, torch_dtype="auto")
+
+    @property
+    def _mode_options(self):
+        return {
+            TaskType.SEQUENCE_CLASSIFICATION: AutoModelForSequenceClassification,
+            TaskType.TEXT_GENERATION: AutoModelForCausalLM,
+            TaskType.TEXT_SUMMARISATION: AutoModelForSeq2SeqLM,
+        }
+
+    def tokenizer_function(self, dataset: DatasetDict, text_column: str = "text"):
+        """Tokenizer function for the dataset."""
+        return self.tokenizer(dataset[text_column], truncation=True)
+
+    def tokenize(self, limit=True):
+        """Tokenize the input data."""
+        tokenized_dataset = self.dataset.map(self.tokenizer_function, batched=True)
+
+        if limit:
+            limited_train_dataset = tokenized_dataset["train"].shuffle(seed=42).select(range(1000))
+            limited_eval_dataset = tokenized_dataset["test"].shuffle(seed=42).select(range(1000))
+            return limited_train_dataset, limited_eval_dataset
+
+        return tokenized_dataset["train"], tokenized_dataset["test"]
--- tests/test_fine_tuning.py
+++ tests/test_fine_tuning.py
@@ -1,9 +1,23 @@
 """Test fine tuning pipeline."""
 
-import pytest
+from datasets import load_dataset
+
+from src.fine_tuning_pipeline import FineTunerPipeline, TaskType
 
 
-@pytest.mark.skip("To be implemented")
 def test_fine_tuning():
     """Test fine tuning pipeline."""
+    dataset = load_dataset("xsum")
+    _ = FineTunerPipeline(
+        model_name="gpt2",
+        dataset=dataset,
+        mode=TaskType.TEXT_SUMMARISATION,
+        fine_tuning_config={"text_column": "document", "target_column": "summary"},
+    )
+    breakpoint()
+    return None
+
+
+def test_peft():
+    """Test PEFT pipeline."""
     return None
